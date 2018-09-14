c67d49e042a1bd95b9358fa15c8515e25a645be7
isac <isacrodrigues2011@live.com>
2018-09-14 16:45:25 -0300

    css adicionado

--- loja/urls.py
+++ loja/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.products_list, name='products_list') 
-    #path('', views.Pagina_produto, name='Pagina_produto'),
+  
 
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 def products_list(request):
     return render(request, 'loja/products_list.html', {})
-'''def Pagina_produto(request):
-	return render(request'loja/Pagina_produto', {})
+
