11e5429dfbdfb9305f8ddff4d9b58d180cd6c4d2
isac <isacrodrigues2011@live.com>
2018-09-19 01:05:50 -0300

    site layout final

--- loja/urls.py
+++ loja/urls.py
@@ -10,7 +10,19 @@ urlpatterns = [
     path('products_consult_bones.html', views.products_consult_bones, name='products_consult_bones.html'),
     path('products_consult_bermudas.html', views.products_consult_bermudas, name='products_consult_bermudas.html'),
     path('products_consult_sapatos.html', views.products_consult_sapatos, name='products_consult_sapatos.html'), 
-  	path('products_page.html', views.products_page, name='products_page.html') 
+  	path('products_page.html', views.products_page, name='products_page.html'),
+  	path('paginas_produtos/products_page_camisa_rosa.html', views.products_page_camisa_rosa, name='products_page_camisa_rosa.html'),
+  	path('paginas_produtos/products_page_camisa_vermelha.html', views.products_page_camisa_vermelha, name='products_page_camisa_vermelha.html'),
+  	path('paginas_produtos/products_page_camisa_verde.html', views.products_page_camisa_verde, name='products_page_camisa_verde.html'),
+    path('paginas_produtos/products_page_bone_vermelho.html', views.products_page_bone_vermelho, name='products_page_bone_vermelho.html'),
+    path('paginas_produtos/products_page_bone_verde.html', views.products_page_bone_verde, name='products_page_bone_verde.html'),
+    path('paginas_produtos/products_page_bone_cinza.html', views.products_page_bone_cinza, name='products_page_bone_cinza.html'),
+    path('paginas_produtos/products_page_bermuda_bege.html', views.products_page_bermuda_bege, name='products_page_bermuda_bege.html'),
+    path('paginas_produtos/products_page_bermuda_cinza.html', views.products_page_bermuda_cinza, name='products_page_bermuda_cinza.html'),
+    path('paginas_produtos/products_page_bermuda_jeans.html', views.products_page_bermuda_jeans, name='products_page_bermuda_jeans.html'),
+    path('paginas_produtos/products_page_sapato_bege.html', views.products_page_sapato_bege, name='products_page_sapato_bege.html'),
+    path('paginas_produtos/products_page_sapato_marrom.html', views.products_page_sapato_marrom, name='products_page_sapato_marrom.html'),
+    path('paginas_produtos/products_page_tenis_preto.html', views.products_page_tenis_preto, name='products_page_tenis_preto.html')
 
 ] 
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- loja/views.py
+++ loja/views.py
@@ -3,6 +3,12 @@ from django.db import transaction
 from django.db import models
 from .models import Produto
 
+
+
+
+def products_list(request):
+    return render(request, 'loja/products_list.html', {})
+
 def products_consult(request):
     return render(request, 'loja/products_consult.html', {})
 def products_consult_camisas(request):
@@ -17,16 +23,44 @@ def products_consult_bermudas(request):
 def products_consult_sapatos(request):
     return render(request,'loja/products_consult_sapatos.html',{})
 
-
-
 def products_page(request):
     return render(request, 'loja/products_page.html', {})
 
+def products_page_camisa_verde(request):
+    return render(request, 'loja/paginas_produtos/products_page_camisa_verde.html', {})
 
-    
+def products_page_camisa_vermelha(request):
+    return render(request, 'loja/paginas_produtos/products_page_camisa_vermelha.html', {})
 
-def products_list(request):
-    return render(request, 'loja/products_list.html', {})
+def products_page_camisa_rosa(request):
+    return render(request, 'loja/paginas_produtos/products_page_camisa_rosa.html', {})
+
+def products_page_bermuda_jeans(request):
+    return render(request, 'loja/paginas_produtos/products_page_bermuda_jeans.html', {})
+
+def products_page_bermuda_bege(request):
+    return render(request, 'loja/paginas_produtos/products_page_bermuda_bege.html', {})
+
+def products_page_bermuda_cinza(request):
+    return render(request, 'loja/paginas_produtos/products_page_bermuda_cinza.html', {})
+
+def products_page_sapato_bege(request):
+    return render(request, 'loja/paginas_produtos/products_page_sapato_bege.html', {})
+
+def products_page_sapato_marrom(request):
+    return render(request, 'loja/paginas_produtos/products_page_sapato_marrom.html', {})    
+
+def products_page_tenis_preto(request):
+    return render(request, 'loja/paginas_produtos/products_page_tenis_preto.html', {})
+
+def products_page_bone_vermelho(request):
+    return render(request, 'loja/paginas_produtos/products_page_bone_vermelho.html', {})
+
+def products_page_bone_cinza(request):
+    return render(request, 'loja/paginas_produtos/products_page_bone_cinza.html', {})
+
+def products_page_bone_verde(request):
+    return render(request, 'loja/paginas_produtos/products_page_bone_verde.html', {})
 
 
  		
